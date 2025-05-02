b68664884f8f6383e0c6102534b0c8a9bebca042
R-Almousa <rakanee12@gmail.com>
2025-05-02 22:49:53 +0300

    Update Photo.java
    complet Photo class

--- Photo.java
+++ Photo.java
@@ -1,27 +1,58 @@
 public class Photo {
-    String path;
-    LinkedList<String> tags;
 
-    // Constructor
-    public Photo(String path, LinkedList<String> tags) {
+    private String path;
+    LinkedList<String> allTags = new LinkedList<>();
+
+    public Photo(String path, LinkedList<String> tags)
+    {
         this.path = path;
-        this.tags = tags;
+
+        if (! tags.empty())
+        {
+            tags.findFirst();
+            while ( !tags.last())
+                allTags.insert(tags.retrieve());
+                tags.findNext();
+            }
+            allTags.insert(tags.retrieve());/
+        }
     }
 
-    // Return the path (full file name) of the photo
-    public String getPath() {
+    public String getPath()
+    {
         return path;
     }
 
-    // Return all tSags associated with the photo
-    public LinkedList<String> getTags() {
-        return tags;
+    public LinkedList<String> getTags()
+    {
+        LinkedList<String> tagsNew = new LinkedList<String>();
+
+        if (! allTags.empty())
+        {
+            allTags.findFirst();
+            while ( ! allTags.last())
+            {
+                tagsNew.insert(allTags.retrieve());
+                allTags.findNext();
+            }
+            tagsNew.insert(allTags.retrieve());
+        }
+        return tagsNew;
     }
 
-    public void displayPhoto() {
-    System.out.println("path=" + path);
-    System.out.print("tags is: ");
-    tags.display();
+    @Override
+    public String toString() {
+
+        String str = "Photo{" + "path=" + path + ", allTags=" ;
+
+        allTags.findFirst();
+        while ( ! allTags.last())
+        {
+            str += allTags.retrieve().toString() + "; ";
+            allTags.findNext();
+        }
+
+        str += allTags.retrieve().toString()  + "}";
+        return str ;
+    }
 }
-
-    }
